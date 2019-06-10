11b0b2337d497a45c66338361d14196f602469e5
David Votrubec <david.votrubec@gmail.com>
2019-06-10 21:29:15 +0200

    Added tests for Graph of Averages. Installed package ng-mock for mocking components

--- src/app/graph-of-averages/graph-of-averages.component.spec.ts
+++ src/app/graph-of-averages/graph-of-averages.component.spec.ts
@@ -1,26 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { GraphOfAveragesComponent } from './graph-of-averages.component';
+import { Spectator, createTestComponentFactory } from '@netbasal/spectator';
+import { MockComponent } from 'ng-mocks';
+import { HighchartsChartComponent } from 'highcharts-angular';
 
 describe('GraphOfAveragesComponent', () => {
-  let component: GraphOfAveragesComponent;
-  let fixture: ComponentFixture<GraphOfAveragesComponent>;
+  let spectator: Spectator<GraphOfAveragesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      declarations: [ GraphOfAveragesComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GraphOfAveragesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  const createComponent = createTestComponentFactory({
+    component: GraphOfAveragesComponent,
+    imports: [HttpClientModule],
+    providers: [HttpClientModule],
+    declarations: [MockComponent(HighchartsChartComponent)]
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    spectator = createComponent();
+  });
+
+  it('should disable button when clicked', () => {
+    let button = spectator.query('button');
+    expect(button.hasAttribute('disabled')).toBeFalsy();
+
+    button.dispatchEvent(new MouseEvent('click'));
+    spectator.detectChanges();
+
+    // Re-select the button, so I have up-to-date values
+    button = spectator.query('button');
+    expect(button.hasAttribute('disabled')).toBeTruthy();
+  });
+
+  it('should invoke method when button is clicked', () => {
+    const button = spectator.query('button');
+    spyOn(spectator.component, 'calculateAllAverages');
+
+    button.dispatchEvent(new MouseEvent('click'));
+    spectator.detectChanges();
+
+    expect(spectator.component.calculateAllAverages).toHaveBeenCalled();
   });
 });
